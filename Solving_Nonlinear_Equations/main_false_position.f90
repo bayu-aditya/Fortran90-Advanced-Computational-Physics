44bd0a54f406dce859f45e97107a85c43406504b
Bayu <bayu.aditya@sci.ui.ac.id>
2019-03-17 18:55:27 +0700

    Main Program False Position

---
+++ Solving_Nonlinear_Equations/main_false_position.f90
@@ -0,0 +1,11 @@
+program metode_false_position
+    implicit none
+    real(4) :: test4
+    real(8) :: test8
+
+    test4 = 1.000000001
+    test8 = 1.000000001
+    write(*,*) test4
+    write(*,*) test8
+
+end program metode_false_position
