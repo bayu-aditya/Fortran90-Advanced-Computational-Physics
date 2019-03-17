329fabd97b32367b120adae71b0c0126879da8c3
Bayu <bayu.aditya@sci.ui.ac.id>
2019-03-17 19:10:48 +0700

    testing Double precision

--- Solving_Nonlinear_Equations/main_false_position.f90
+++ Solving_Nonlinear_Equations/main_false_position.f90
@@ -1,11 +1,17 @@
 program metode_false_position
     implicit none
-    real(4) :: test4
-    real(8) :: test8
+    integer, parameter :: dpr = kind(1.0D0)
 
-    test4 = 1.000000001
-    test8 = 1.000000001
-    write(*,*) test4
-    write(*,*) test8
+    real :: angka_single
+    real(dpr) :: angka_double
+    integer :: i
+
+    angka_double = 0
+
+    do i = 1,20
+        angka_single = i**10
+        angka_double = i**10_dpr
+        write(*,*) i, angka_single, angka_double
+    end do
 
 end program metode_false_position
